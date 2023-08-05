c709444ca9f9003a549786ff9bc6f0295ba2342e
Max Golotsvan <maxgolo89@gmail.com>
2023-08-05 09:48:54 +0300

    Fix names

---
+++ PriorityQueue.hpp
@@ -0,0 +1,11 @@
+//
+// Created by maxg on 8/5/23.
+//
+
+#ifndef DATA_STRUCTURES_PRIORITYQUEUE_HPP
+#define DATA_STRUCTURES_PRIORITYQUEUE_HPP
+namespace NotStd
+{
+    
+}
+#endif //DATA_STRUCTURES_PRIORITYQUEUE_HPP
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 # data-structures
-=======
-Still empty
->>>>>>> 56301f5 (First commit)
+
